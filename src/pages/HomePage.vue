24f452b2dc693674933c1ce370232cdc5452f5f9
Hirotada Takeuchi <t.hir0tada.biz@gmail.com>
2023-03-31 12:07:24 +0900

    feat: own values thumbnail

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,26 @@
 <template>
-  <q-page class="row items-center justify-evenly text-h3"> Contents </q-page>
+  <q-page class="q-py-md">
+    <h2 class="q-mt-none text-h5">
+      Your Own Value
+      <q-btn icon="mdi-help-circle-outline" round flat dense class="q-ml-md" />
+    </h2>
+    <div class="row q-col-gutter-sm">
+      <div class="col-4" v-for="(val, index) of arr" :key="index">
+        <q-card class="bg333" style="border-radius: 15px">
+          <q-img :src="`/img/values/${val.toLowerCase()}.png`">
+            <div
+              class="absolute-bottom text-center text-dark text-weight-bold"
+              style="background: none; padding-bottom: 5px"
+            >
+              {{ val }}
+            </div>
+          </q-img>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const arr = ['Challenge', 'Burning', 'Explore'];
+</script>
